05438a88eedbd270e87b9f79d4c23e38beedbdf2
vir2s <virtualik@gmail.com>
2021-03-18 14:10:42 +0200

    download func added

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 TITLE = "=>[ Image Downloader by Vitaly Sem ]>="
 ICON = "gui/icon.png"
 SCREEN_SIZE = ""
+DIR = "downloads/"
+DOWNLOAD_URLS = [
+    "https://www.gogetnews.info/uploads/posts/2021-03/1616060682_krym.jpg",
+    "https://www.gogetnews.info/uploads/posts/2021-03/1616061185_rada.jpg",
+    "https://www.gogetnews.info/uploads/posts/2021-03/1616056491_moz.jpeg",
+    "https://www.gogetnews.info/uploads/posts/2021-03/1615989208_izobrazhenie_2021-03-17_155148.png",
+    "https://www.gogetnews.info/uploads/posts/2021-03/1615989514_bayden.jpg"
+]
+
--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+import urllib.request
 from config import *
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QLineEdit, QTextEdit, QGridLayout, QWidget, QListWidget
@@ -21,16 +22,24 @@ class Window(QWidget):
         self.urls_entry = QListWidget()
         self.url_entry = QLineEdit("URL", self)
         self.add_button = QPushButton("Add", self)
+        self.download_button = QPushButton("Download", self)
 
         self.layout = QGridLayout()
         self.layout.addWidget(self.url_entry)
         self.layout.addWidget(self.add_button)
         self.layout.addWidget(self.urls_entry)
+        self.layout.addWidget(self.download_button)
 
         self.setLayout(self.layout)
 
         self.add_button.clicked.connect(self.add_url)
+        self.download_button.clicked.connect(self.download)
 
     def add_url(self):
         self.urls.append(self.url_entry.text())
         self.urls_entry.addItem(self.url_entry.text())
+
+    def download(self):
+        for url in self.urls:
+            name = url.split("/")[-1]
+            urllib.request.urlretrieve(url, DIR + name)
